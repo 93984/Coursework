9cb4ee74811bea777c48c396466b22b7b669c29b
93984 <93984@farnborough.ac.uk>
2020-10-01 11:23:41 +0100

    Web server setup

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -19,34 +19,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        openDatabase("Databases.db");                     //connect to our database file, when you stop the server the connection to the database
-        // is closed and you can access it through SQLite Studio
+        openDatabase("Databases.db");
+        ResourceConfig config = new ResourceConfig();
+        config.packages("controllers");
+        config.register(MultiPartFeature.class);
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
 
-        ResourceConfig config = new ResourceConfig();       // prepare our Jersey Servlet, 'Servlet' is a Java program that runs on a Java-enabled web servers.
-        // Jersey is our Servlet Library, Jetty is our Server Library
-
-        config.packages("controllers");                     // part of the server that provides API, listening and responding to HTTP requests
-        config.register(MultiPartFeature.class);            // support multipart HTML forms
-        ServletHolder servlet = new ServletHolder(new ServletContainer(config));  //Instantiate the Servlet
-
-        Server server = new Server(8081);                   // prepare our Jetty Server to listen on port 8081
-        ServletContextHandler context = new ServletContextHandler(server, "/");  // instantiate the Server
-        context.addServlet(servlet, "/*");                  // connect the Servlet to the Server
+        Server server = new Server(8081);
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(servlet, "/*");
 
         try {
-            server.start();                                 // start the server
+            server.start();
             System.out.println("Server successfully started.");
-            server.join();                                  // Wait for the server to rejoin the main thread, program waits here
+            server.join();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     public static void openDatabase(String dbFile) {
+
         try {
             SQLiteConfig config = new SQLiteConfig();
-            config.enforceForeignKeys(true);                //need this to cascade delete/update in database
+            config.enforceForeignKeys(true);
             Class.forName("org.sqlite.JDBC");
             db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
             System.out.println("Database connection successfully established.");

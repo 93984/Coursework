af7b0e700a94063315ca298fbfe8265cce77f75d
aidan <aidantuckeraidant@gmail.com>
2020-12-31 19:38:17 +0000

    push

--- resources/client/js/wordgame.js
+++ resources/client/js/wordgame.js
@@ -4,6 +4,7 @@ let guessDecrement = 1000;
 const extraGuessDecrement = 125;
 let attempts = 0;
 const maxAttempts = 16;
+let interval = 50;
 let youGotIt = false;
 
 /*-------------------------------------------------------
@@ -25,6 +26,20 @@ function getQueryStringParameters() {
     return params;
 }
 
+function lowercase(userInput) {
+    let yourGuess = "";
+
+    for (let i = 0; i < userInput.length; i += 1) {
+        let asc = userInput.charCodeAt(i);
+        if (asc >= 65 && asc <= 90) {            //test for uppercase
+            asc += 32;
+            ``
+        } else if (asc < 97 || asc > 122) {      //test if it is lowercase
+            return ("error");
+        } yourGuess += String.fromCharCode(asc);
+    } return (yourGuess);
+}
+
 function marking (word, yourGuess, youGotIt, attempts, maxAttempts) {
     document.getElementById("pastGuesses").innerHTML = "<h2 style='color: white '>" + yourGuess + "</h2>" + document.getElementById("pastGuesses").innerHTML;
     document.getElementById("guess").value = "";
@@ -38,17 +53,20 @@ function marking (word, yourGuess, youGotIt, attempts, maxAttempts) {
             blacks += 1;
             matchPattern += "●";
             wordTemp = wordTemp.substr(0, i) + "_" + wordTemp.substr(i + 1);
+            yourGuess = yourGuess.substr(0, i) + "-" + yourGuess.substr(i + 1);
             if (blacks === word.length) {
                 youGotIt = true;
                 alert("You got it! The secret word was " + word + ". Your final score is " + scoreTotal);
                 window.location.href="/client/index.html";
+
 
                 //add to leaderboard
             }
         }
     }
     if (attempts === maxAttempts && youGotIt === false) {
-        alert("You have ran out of attempts");
+        alert("You have ran out of attempts! The secret word was " + word);
+        window.location.href="/client/index.html";
     }
     for (let j = 0; j < wordTemp.length; j++) {
         for (let k = 0; k < wordTemp.length; k++) {
@@ -56,7 +74,7 @@ function marking (word, yourGuess, youGotIt, attempts, maxAttempts) {
                 whites += 1;
                 matchPattern += "○";
                 wordTemp = wordTemp.substr(0, k) + "_" + wordTemp.substr(k + 1);
-                yourGuess = yourGuess.substr(0, j) + "_" + yourGuess.substr(j + 1);
+                yourGuess = yourGuess.substr(0, j) + "-" + yourGuess.substr(j + 1);
             }
         }
     }
@@ -101,7 +119,7 @@ function getWord() {
             gameBoardHTML += "</div>";
 
             setInterval(function() {
-                scoreTotal -= 1; document.getElementById("scoreTotal").innerHTML = scoreTotal;}, 40);
+                scoreTotal -= 1; document.getElementById("scoreTotal").innerHTML = scoreTotal;}, 1000/interval);
             if (scoreTotal === 0) {
                 scoreTotal = 0;
             }
@@ -112,10 +130,10 @@ function getWord() {
 
                 if (e.key === 'Enter') {
                     let yourGuess =  e.target.value;
+                    yourGuess = lowercase(yourGuess);
 
                     if (yourGuess.length !== word.length) {
-                        alert("That isn't " + word.length + " letters long!");
-
+                        alert("That isn't " + word.length + " letters long or you have entered an invalid character!");
                     } else {
                         attempts += 1;
                         marking(word, yourGuess, youGotIt, attempts, maxAttempts);
